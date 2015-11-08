9ae3e1d7dc45f6b662ef6bf6b83ca581c3fc18f7
Juan Jose Guerrero Madrigal <Juan Jose Guerrero Madrigal>
2015-11-07 21:38:37 -0600

    Añadido get awardPerson/awardTeam y check de gets

--- scripts/scripts functions specific.sql
+++ scripts/scripts functions specific.sql
@@ -24,26 +24,54 @@ CREATE PROCEDURE getIdTeam(in pIdPlayer INT, in pIdGame INT)
  END //
 DELIMITER ;
 
- 
+select * from mydb.Event;
+
+call getGroupsTeams(2)
+drop PROCEDURE getGroupsTeams
 DELIMITER //
 CREATE PROCEDURE getGroupsTeams(in pIdEvent INT)
  BEGIN
-	select idGroup , nameGroup, idTeam, nameTeam
+	select gr.idGroup , gr.nameGroup, te.idTeam, te.nameTeam
     from mydb.Group gr inner join Team te on gr.idEvent = pIdEvent and te.idGroup = gr.idGroup;
  END //
 DELIMITER ;
 
- 
+select * from Team;
+select * from Country;
+call getAwardsPerCountry(4);
+
+
+drop PROCEDURE getAwardsPerCountry;
 DELIMITER //
 CREATE PROCEDURE getAwardsPerCountry(in pIdCountry INT)
  BEGIN
-	select aw.idAward , aw.nameAward 
-	from AwardTeam aw, Country co inner join Team te
+	select awt.nameAward 
+	from AwardTeam awt, Award_Team aw, Country co inner join Team te
     on co.idCountry = te.idCountry
-    where aw.idTeam = te.idTeam;
+    where aw.idTeam = te.idTeam and aw.idAwardTeam = awt.idAwardTeam;
  END //
 DELIMITER ;
- 
+
+select * from Player;
+select * from AwardPerson;
+call insertAwardxPlayer(1,1,2);
+select * from Award_Player;
+call getAwardPerPerson(1);
+
+drop PROCEDURE getAwardPerPerson;
+DELIMITER //
+CREATE PROCEDURE getAwardPerPerson(in pIdPlayer INT)
+ BEGIN
+	select awp.nameAwardPerson 
+	from AwardPerson awp, Award_Player aw
+    where aw.idPlayer = pIdPlayer and awp.idAwardPerson = aw.idAwardPerson;
+ END //
+DELIMITER ;
+
+
+
+
+call getCountry(); 
 DELIMITER //
 CREATE PROCEDURE getCountry()
  BEGIN
@@ -52,7 +80,7 @@ CREATE PROCEDURE getCountry()
  END //
 DELIMITER ;
 
-
+call getCity()
 DELIMITER //
 CREATE PROCEDURE getCity()
  BEGIN
@@ -62,6 +90,8 @@ CREATE PROCEDURE getCity()
 DELIMITER ;
 
 
+
+call getTeamsPerGroup(1)
 DELIMITER //
 CREATE PROCEDURE getTeamsPerGroup(in pIdGroup INT)
  BEGIN
@@ -72,14 +102,13 @@ CREATE PROCEDURE getTeamsPerGroup(in pIdGroup INT)
 DELIMITER ;
 
 
-select * from Person
-
+drop procedure getPlayer;
 DELIMITER //
 CREATE PROCEDURE getPlayer(in pIdCountry int)
  BEGIN
-	select pl.idPlayer , pe.firstName, pe.secondName, pe.lastName, pl.height, pl.weight, getPlayerAge()
+	select pl.idPlayer , pe.firstName, pe.secondName, pe.lastName, pl.height, pl.weight, getPlayerAge(pe.birthdate)
 	from Player pl, Person pe
-    inner join Country co on co.idCountry = pe.idCountry
+    inner join Country co on co.idCountry = pe.idCountry and pe.idCountry = pIdCountry
     where pl.idPerson = pe.idPerson;
  END //
 DELIMITER ;
@@ -99,6 +128,15 @@ call getGamePerEvent();
 
 
 
+DELIMITER //
+CREATE PROCEDURE getGamePerEvent()
+ BEGIN
+	select ev.nameEvent, ev.idEvent , ga.idGame , vis.nameTeam , hom.nameTeam
+    from Team vis, Team hom,mydb.Event ev inner join Game ga on ga.idEvent = ev.idEvent
+    where vis.idTeam = ga.idVisitor  and hom.idTeam = ga.idHome
+    group by idGame , vis.NameTeam , hom.NameTeam;
+ END //
+DELIMITER ;
 
 
 
